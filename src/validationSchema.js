3ed513128e5073627668c5c0f3a6d67dd8710905
Vanmiganathan <vanmiganathans@bloomlync.com>
2022-06-06 00:51:31 +0530

    ok

--- src/validationSchema.js
+++ src/validationSchema.js
@@ -1,13 +1,13 @@
 import * as Yup from "yup";
 
+const formSchema = Yup.object().shape({
+  name: Yup.string().required("Required"), // these constraints take precedence
+  age: Yup.string().required("Required"), // these constraints take precedence
+});
+
 export const schema = Yup.object().shape({
   friends: Yup.array()
-    .of(
-      Yup.object().shape({
-        name: Yup.string().required("Required"), // these constraints take precedence
-        age: Yup.string().required("Required"), // these constraints take precedence
-      })
-    )
+    .of(formSchema)
     .required("Must have friends") // these constraints are shown if and only if inner constraints are satisfied
     .min(1, "Minimum of 1 friends"),
 });
