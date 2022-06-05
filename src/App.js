6fb1439fb776018223174ef2a19d695bd9eb3862
Vanmiganathan <vanmiganathans@bloomlync.com>
2022-06-06 00:15:42 +0530

    kjh

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import { Formik, Form, Field, FieldArray } from "formik";
 
-// Here is an example of a form with an editable list.
-// Next to each input are buttons for insert and remove.
-// If the list is empty, there is a button to add an item.
 export const App = () => (
   <div>
     <h1>Friend List</h1>
     <Formik
-      initialValues={{ friends: ["jared", "ian", "brent"] }}
+      initialValues={{ friends: [{ name: "", age: "" }] }}
       onSubmit={(values) =>
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2));
@@ -20,31 +17,28 @@ export const App = () => (
             name="friends"
             render={(arrayHelpers) => (
               <div>
-                {values.friends && values.friends.length > 0 ? (
-                  values.friends.map((friend, index) => (
-                    <div key={index}>
-                      <Field name={`friends.${index}`} />
-                      <button
-                        type="button"
-                        onClick={() => arrayHelpers.remove(index)} // remove a friend from the list
-                      >
-                        -
-                      </button>
-                      <button
-                        type="button"
-                        onClick={() => arrayHelpers.insert(index, "")} // insert an empty string at a position
-                      >
-                        +
-                      </button>
-                    </div>
-                  ))
-                ) : (
-                  <button type="button" onClick={() => arrayHelpers.push("")}>
-                    {/* show this when user has removed all friends from the list */}
-                    Add a friend
-                  </button>
-                )}
+                {values.friends.map((friend, index) => (
+                  <div key={index}>
+                    {/** both these conventions do the same */}
+                    <Field name={`friends[${index}].name`} />
+                    <Field name={`friends.${index}.age`} />
+
+                    <button
+                      type="button"
+                      onClick={() => arrayHelpers.remove(index)}
+                    >
+                      -
+                    </button>
+                  </div>
+                ))}
+                <button
+                  type="button"
+                  onClick={() => arrayHelpers.push({ name: "", age: "" })}
+                >
+                  +
+                </button>
                 <div>
+                  <br/>
                   <button type="submit">Submit</button>
                 </div>
               </div>
@@ -56,4 +50,3 @@ export const App = () => (
   </div>
 );
 export default App;
-
